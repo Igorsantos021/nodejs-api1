293d655ca286ff1804977929ef28c327f98832b5
Igor Santos <santosigor868@gmail.com>
2024-06-24 21:01:44 -0300

    metodo GET listagem herois

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const app = express()
 
-app.get("/", function(req, res){
-    res.send("Hello Wordl")
+
+const herois = ["homen aranha","hulk","ms.marvel"]
+
+//ENDPOINT listagem de todos os herois (READ ALL -> [GET])
+
+app.get("/heroes", function(req,res){
+    res.send(herois)
 })
 
-app.get("/ptbr", function(req, res){
-    res.send("Olá Mundo")
+app.get("/heroes/:id", function(req,res){
+    //acesar o paramentro de rota ID
+    const id = req.params.id
+
+    //Pego o item no array
+    const umheroi = herois[id]
+
+    //envio o item encontrado como resposta
+    res.send(umheroi)
 })
 
+
+/*
 app.post()
 app.put()
 app.patch()
 app.delete()
+*/
 
 
 
